e5aff22dfc0ba6a9026680854b7c813657b46f50
TCollins2001 <tc486@njit.edu>
2025-08-04 22:02:16 -0400

    added "list tasks" function

--- SimpleToDoList.java
+++ SimpleToDoList.java
@@ -20,11 +20,23 @@ public class SimpleToDoList {
         System.out.println("Enter Task: ");
         String taskDesc = scanner.nextLine().trim();
         if (taskDesc.isEmpty()) {
-            System.out.println("No Tasks Yet!");
+            System.out.println("Empty Task! Try Again.");
             return;
         }
         tasks.add(new Task(taskDesc));
         System.out.println("Task Added!");
+    }
+
+    static void listTasks(List<Task> tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("No Tasks Yet!");
+            return;
+        } else {
+            System.out.println("Your Tasks!");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i+1) + ". " + tasks.get(i));
+            }
+        }
     }
 
     public static void main(String[] args) {
