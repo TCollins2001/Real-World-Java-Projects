961867066ef8d3b92dd7f6d7c97982bb58d60d5c
TCollins2001 <tc486@njit.edu>
2025-08-07 19:13:02 -0400

    added function for users to withdraw

--- BankAccount.java
+++ BankAccount.java
@@ -11,9 +11,9 @@ class Bank {
 
 public class BankAccount {
 
-    static void viewBalance (Bank bank) {
-        System.out.printf("Your Balance: $%.2f", bank.userBalance);
-
+    static void viewBalance(Bank bank) {
+        System.out.printf("Your Balance: $%.2f\n", bank.userBalance);
+        System.out.println();
     }
 
     static void makeDeposit(Bank bank, Scanner scanner) {
@@ -21,8 +21,9 @@ public class BankAccount {
         while (true) {
             System.out.println();
 
-            System.out.print("Enter Deposit: ");
+            System.out.print("Enter Deposit Amount (Using Two Decimal Places): ");
             double userDeposit = scanner.nextDouble();
+            scanner.nextLine();
 
             if (userDeposit <= 0) {
                 System.out.println("Invalid Entry. Try Again.");
@@ -35,15 +36,45 @@ public class BankAccount {
 
                 System.out.println();
 
-                System.out.printf("New Balance: $%.2f", bank.userBalance);
+                System.out.printf("New Balance: $%.2f\n", bank.userBalance);
+
+                System.out.print("Do You Want To Add More? (Y/N): ");
+                char userAddMore = scanner.next().trim().toUpperCase().charAt(0);
+                scanner.nextLine();
+                System.out.println();
+
+                if (userAddMore != 'Y') {
+                    break;
+                }
+            }
+        }
+    }
+
+    static void makeWithdraw(Bank bank, Scanner scanner) {
+
+        while (true) {
+            System.out.println("Enter Withdrawal Amount (Using Two Decimal Places): ");
+            double userWithdraw = scanner.nextDouble();
+            scanner.nextLine();
+
+            if (userWithdraw <= 0) {
+                System.out.println("Invalid Entry. Try Again.");
+            } else if (userWithdraw > bank.userBalance) {
+                System.out.println("Overdraft. Try Again.");
+            } else {
+                bank.userBalance -= userWithdraw;
+                System.out.println("Withdrawal Successful!");
 
                 System.out.println();
 
-                System.out.print("Do You Want To Add More? (Y/N) ");
-                char useraddMore = scanner.next().trim().toUpperCase().charAt(0);
+                System.out.printf("New Balance: $%.2f\n", bank.userBalance);
+
+                System.out.print("Do You Want To Withdraw More? (Y/N): ");
+                char userWithdrawMore = scanner.next().trim().toUpperCase().charAt(0);
+                scanner.nextLine();
                 System.out.println();
 
-                if (useraddMore != 'Y') {
+                if (userWithdrawMore != 'Y') {
                     break;
                 }
             }
@@ -55,12 +86,17 @@ public class BankAccount {
         Bank bank = new Bank(0.00);
 
        while (true) {
+
+           System.out.println();
+
            System.out.println("1. View Balance");
            System.out.println("2. Add Deposit");
            System.out.println("3. Withdraw");
            System.out.println("4. Exit");
 
-           System.out.println("Enter Number Of Choice: ");
+           System.out.println();
+
+           System.out.print("Enter Number Of Choice: ");
            String userChoice = scanner.nextLine().trim();
 
            switch (userChoice) {
@@ -71,7 +107,14 @@ public class BankAccount {
                    makeDeposit(bank, scanner);
                    break;
                case "3":
+                   makeWithdraw(bank, scanner);
+                   break;
                case "4":
+                   System.out.println("Exited :)");
+                   scanner.close();
+                   return;
+               default:
+                   System.out.println("Invalid Choice. Try Again.");
            }
        }
     }
