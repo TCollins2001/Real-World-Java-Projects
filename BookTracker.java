0bd4aa23729e88d7faeda96c59fe3cd94c1fdab4
TCollins2001 <tc486@njit.edu>
2025-08-12 19:18:54 -0400

    start of book tracker, added view list of books function for users

---
+++ BookTracker.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Book {
+
+    String bookTitle;
+
+    Book(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    @Override
+    public String toString() {
+        return bookTitle;
+    }
+}
+
+public class BookTracker {
+
+    static void listBooks(List<Book> bookList) {
+        System.out.println();
+        System.out.println("Our Book List: ");
+        System.out.println("---------------");
+        for (Book book : bookList) {
+            System.out.println(book.bookTitle);
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book("The Lord of the Rings"));
+        bookList.add(new Book("The Da Vinci Code"));
+        bookList.add(new Book("To Kill a Mockingbird"));
+        bookList.add(new Book("Harry Potter and the Sorcerer's Stone"));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1. View Book List");
+        System.out.println("2. Exit");
+
+        System.out.println();
+    }
+}
