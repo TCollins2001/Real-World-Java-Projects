cd125a55af9cf1a707e466a61422bde7ec083c55
TCollins2001 <tc486@njit.edu>
2025-08-19 02:39:32 -0400

    start of hotel booking management system, adding variables

---
+++ HotelBooking.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Management {
+
+    int roomNum;
+    String roomType;
+    String pricePerNight;
+    boolean isAvailable;
+
+    Management(int roomNum, String roomType, String pricePerNight, boolean isAvailable) {
+
+        this.roomNum = roomNum;
+        this.roomType = roomType;
+        this.pricePerNight = pricePerNight;
+        this.isAvailable = true;
+    }
+}
+
+public class HotelBooking {
+    public static void main(String[] args) {
+
+    }
+}
+
