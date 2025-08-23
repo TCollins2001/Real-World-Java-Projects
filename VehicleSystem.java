7eb0dfa0c3f05fd2211e772847c6c057feb7c03a
TCollins2001 <tc486@njit.edu>
2025-08-23 18:54:24 -0400

    added car class extension to vehicle, getters & setters, and override display method

--- VehicleSystem.java
+++ VehicleSystem.java
@@ -38,3 +38,38 @@ class Vehicle {
                 "Year: " + year;
     }
 }
+
+class Car extends Vehicle {
+
+    int numDoors;
+    boolean isElectric;
+
+    Car(int numDoors, boolean isElectric, String make, String model, int year) {
+        super(make, model, year);
+        this.numDoors = numDoors;
+        this.isElectric = isElectric;
+    }
+
+    int getNumDoors() {
+        return numDoors;
+    }
+
+    void setNumDoors(int numDoors) {
+        this.numDoors = numDoors;
+    }
+
+    boolean getIsElectric() {
+        return isElectric;
+    }
+
+    void setIsElectric(boolean isElectric) {
+        this.isElectric = isElectric;
+    }
+
+    @Override
+    public String detailsDisplay() {
+        return super.detailsDisplay() + "\n" +
+                "Number of Doors: " + "\n" +
+                "Electric?: " + isElectric;
+    }
+}
