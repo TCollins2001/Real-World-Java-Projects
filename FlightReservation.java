401fd763fc7bf258605089ac68d9bec4551ac3df
TCollins2001 <tc486@njit.edu>
2025-08-29 14:20:02 -0400

    added bookFlight method (unfinished) and tested

--- FlightReservation.java
+++ FlightReservation.java
@@ -19,8 +19,28 @@ class PassengerInfo {
 
 class FlightReservation {
 
+    static void bookFlight(List<PassengerInfo> pInfoList, Scanner scanner) {
+        System.out.println();
+        System.out.print("Enter Passenger Name: ");
+        String userName = scanner.nextLine().trim();
+        System.out.println();
+        System.out.print("Enter Destination: ");
+        String userDestination = scanner.nextLine().trim();
+
+        PassengerInfo newPassenger = new PassengerInfo(userName, userDestination);
+        if (userName.isEmpty() || userDestination.isEmpty()) {
+            System.out.println("Empty Entry. Try Again.");
+        } else {
+            pInfoList.add(newPassenger);
+            System.out.println();
+            System.out.println("Flight Booked Successfully!");
+        }
+    }
+
     public static void main(String[] args) {
         List<PassengerInfo> pInfoList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+
+        bookFlight(pInfoList, scanner);
     }
 }
