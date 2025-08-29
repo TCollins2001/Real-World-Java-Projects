1c89d3bfdab0ba91ead77bd29b11766be8fdda71
TCollins2001 <tc486@njit.edu>
2025-08-29 14:34:17 -0400

    added viewFlightInfo method to check passenger info and switch case for menu

--- FlightReservation.java
+++ FlightReservation.java
@@ -37,10 +37,41 @@ class FlightReservation {
         }
     }
 
+    static void viewFlightInfo(List<PassengerInfo> pInfoList) {
+        System.out.println();
+        if (pInfoList.isEmpty()) {
+            System.out.println("No Flights Booked Yet!");
+        } else {
+            for (PassengerInfo p : pInfoList) {
+                System.out.println("Flight Information: ");
+                System.out.println("---------------------");
+                System.out.println(p);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         List<PassengerInfo> pInfoList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        bookFlight(pInfoList, scanner);
+        while (true) {
+            System.out.println();
+            System.out.println("1. Book Flight");
+            System.out.println("2. View Flight Information");
+            System.out.println();
+            System.out.print("Enter Choice By Number: ");
+            String userChoice = scanner.nextLine().trim();
+
+            switch (userChoice) {
+                case "1":
+                    bookFlight(pInfoList, scanner);
+                    break;
+                case "2":
+                    viewFlightInfo(pInfoList);
+                    break;
+                default:
+                    System.out.println("Invalid Choice. Try Again.");
+            }
+        }
     }
 }
