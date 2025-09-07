462d1210b4d14ab1587186cd86bd9f6fffa5d2fd
TCollins2001 <tc486@njit.edu>
2025-09-06 23:14:28 -0400

    added selecting a movie method for user to select genre and movie

--- MovieTicketSystem.java
+++ MovieTicketSystem.java
@@ -18,10 +18,43 @@ class MovieDetails {
 
 class MovieTicketSystem {
 
+    static void selectMovie(List<MovieDetails> gList, Scanner scanner) {
+        System.out.println();
+        System.out.println("Genre List: ");
+        System.out.println("-----------");
+        for (int i = 0; i < gList.size(); i++) {
+            System.out.println((i+1) + ". " + gList.get(i));
+        }
+
+        System.out.println();
+
+        System.out.print("Enter Genre Choice By Number: ");
+        int userGenre = scanner.nextInt();
+        scanner.nextLine();
+
+        MovieDetails selectedGenre = gList.get(userGenre - 1);
+
+        if (selectedGenre.genre.isEmpty()) {
+            System.out.println();
+            System.out.println("Empty Entry. Try Again.");
+        } else {
+            System.out.println();
+            System.out.println("Movie List: ");
+            System.out.println("-----------");
+            for (int i = 0; i < selectedGenre.mvList.size(); i++) {
+                System.out.println((i+1) + ". " + selectedGenre.mvList.get(i));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         List<MovieDetails> gList = new ArrayList<>();
         gList.add(new MovieDetails("Horror", Arrays.asList("IT", "Halloween", "At Dawn")));
         gList.add(new MovieDetails("Comedy", Arrays.asList("Dumb & Dumber", "Shazam!", "Brides Maids")));
         gList.add(new MovieDetails("Romance", Arrays.asList("Me Before You", "Our Story", "The Notebook")));
+
+        Scanner scanner = new Scanner(System.in);
+
+        selectMovie(gList, scanner);
     }
 }
