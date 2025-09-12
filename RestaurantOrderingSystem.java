ae41b72ed5b41e00b646d774394bf1d4a2ff2993
TCollins2001 <tc486@njit.edu>
2025-09-11 20:06:42 -0400

    created classes for categories, items, and pricing, as well as menu with switch case

--- RestaurantOrderingSystem.java
+++ RestaurantOrderingSystem.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 class CustomerInformation {
 
@@ -16,8 +17,59 @@ class CustomerInformation {
     }
 }
 
+class OrderItem {
+
+    String category;
+
+}
+
+class Item {
+
+    String itemName;
+    double price;
+
+    Item(String itemName, double price) {
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return itemName + " ($" + String.format("%.2f", price) + ")";
+    }
+}
+
 class RestaurantOrderingSystem {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<CustomerInformation> cInfoList = new ArrayList<>();
+
+        while (true) {
+            System.out.println();
+            System.out.println("1. Order Now");
+            System.out.println("2. Review Order");
+            System.out.println("3. Submit Order");
+            System.out.println("4. Exit Menu");
+            System.out.println();
+            System.out.print("Enter Number Choice: ");
+            String userChoice = scanner.nextLine().trim();
+
+            switch(userChoice) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Invalid Choice. Try Again.");
+            }
+
+        }
 
     }
 }
