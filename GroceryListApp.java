6dcaf3131f7b46935f34074f14991f8899a29a88
TCollins2001 <tc486@njit.edu>
2025-09-13 22:19:37 -0400

    added methods for users to add items to grocery list and view their full grocery list

--- GroceryListApp.java
+++ GroceryListApp.java
@@ -11,12 +11,60 @@ class FoodItem {
 
     @Override
     public String toString() {
-        return name + " (Qty: " + quantity;
+        return name + " (Qty: " + quantity + ")";
     }
 }
 
 class GroceryListApp {
+
+    static void viewList(List<FoodItem> foodItemsList) {
+
+        if (foodItemsList.isEmpty()) {
+            System.out.println();
+            System.out.println("No Items Yet.");
+        } else {
+            System.out.println();
+            System.out.println("Grocery List: ");
+            System.out.println("---------------");
+            for (int i = 0; i < foodItemsList.size(); i++) {
+                System.out.println((i + 1) + ". " + foodItemsList.get(i));
+            }
+        }
+    }
+
+    static void addItem(List<FoodItem> foodItemsList, Scanner scanner) {
+        System.out.println();
+        System.out.print("Add Item: ");
+        String userItem = scanner.nextLine().trim();
+        System.out.println();
+        System.out.print("Enter Quantity: ");
+        int userQuantity = scanner.nextInt();
+        scanner.nextLine();
+
+
+        if (userItem.isEmpty()) {
+            System.out.println();
+            System.out.println("Empty Entry. Try Again.");
+            return;
+        }
+
+        if (userQuantity < 1) {
+            System.out.println();
+            System.out.println("Invalid Quantity. Try Again.");
+            return;
+        }
+
+        foodItemsList.add(new FoodItem(userItem, userQuantity));
+
+        System.out.println();
+        System.out.println("Added Item To List!");
+
+    }
+
+
     public static void main(String[] args) {
+
+        List<FoodItem> foodItemsList = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
 
@@ -32,13 +80,20 @@ class GroceryListApp {
 
             switch (userChoice) {
                 case "1":
+                    viewList(foodItemsList);
                     break;
                 case "2":
+                    addItem(foodItemsList, scanner);
                     break;
                 case "3":
                     break;
                 case "4":
-                    break;
+                    System.out.println();
+                    System.out.println("Exited :)");
+                    return;
+                default:
+                    System.out.println();
+                    System.out.println("Invalid Choice. Try Again.");
             }
         }
     }
