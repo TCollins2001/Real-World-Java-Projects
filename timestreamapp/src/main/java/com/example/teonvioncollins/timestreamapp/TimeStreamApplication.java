b12147d2df79c865826fc5c5427613fa13a375dd
TCollins2001 <tc486@njit.edu>
2025-11-03 18:49:46 -0500

    Remove TimeStream project

---
+++ timestreamapp/src/main/java/com/example/teonvioncollins/timestreamapp/TimeStreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.teonvioncollins.timestreamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeStreamApplication.class, args);
+	}
+
+}
---
+++ timestreamapp/src/test/java/com/example/teonvioncollins/timestreamapp/TimestreamappApplicationTests.java
@@ -0,0 +1,13 @@
+package timestreamapp.src.test.java.com.example.teonvioncollins.timestreamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimestreamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
