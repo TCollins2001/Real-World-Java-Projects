6b1b1de36e7c290378b012a1de66e7ba5b1908a9
TCollins2001 <tc486@njit.edu>
2026-01-19 15:33:55 -0500

    java practice

---
+++ UserTasksTest/src/main/java/com/teonvioncollins/UserTasksTest/UserTasksTestApplication.java
@@ -0,0 +1,13 @@
+package com.teonvioncollins.UserTasksTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTasksTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserTasksTestApplication.class, args);
+	}
+
+}
+
---
+++ UserTasksTest/src/test/java/com/teonvioncollins/UserTasksTest/UserTasksTestApplicationTests.java
@@ -0,0 +1,13 @@
+package UserTasksTest.src.test.java.com.teonvioncollins.UserTasksTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTasksTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
