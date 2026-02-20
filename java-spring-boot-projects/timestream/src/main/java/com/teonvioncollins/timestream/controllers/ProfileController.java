7ed7f7e8907b25dc4bf2ebbfb7297e4d035f861e
TCollins2001 <tc486@njit.edu>
2026-02-19 19:17:53 -0500

    added profile optional doctor design and function

--- java-spring-boot-projects/timestream/src/main/java/com/teonvioncollins/timestream/controllers/ProfileController.java
+++ java-spring-boot-projects/timestream/src/main/java/com/teonvioncollins/timestream/controllers/ProfileController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
